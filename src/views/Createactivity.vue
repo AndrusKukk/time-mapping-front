ac2f1ef357d40934d8ca9ae9e25199275df71b5a
kadkuk <77267320+kadkuk@users.noreply.github.com>
2021-02-16 15:57:46 +0200

    one more minor change

--- src/views/Createactivity.vue
+++ src/views/Createactivity.vue
@@ -59,7 +59,7 @@ export default {
   components: {},
   data: function () {
     return {
-      valid: false,
+      valid: true,
       activities: {
         projectName: '',
         activityName: '',
